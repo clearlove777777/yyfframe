f65947fb64614f860b4a4069a3f1930659f763cf
clearlove777777 <34257750+clearlove777777@users.noreply.github.com>
2017-12-11 09:40:03 +0800

    Update Index.php

--- app/index/controller/Index.php
+++ app/index/controller/Index.php
@@ -22,28 +22,11 @@ class Index extends Controller
 
 	public function index ()
 	{
-		//测试能否使用
-		echo 1;
-		//测试模型类，数据库连接正常，查询语句使用正常
-		//dd(Model::q('select * from tag'));
-		//排序封装
-
-		// $data="select*from student where age>30 order by age desc";
-		dd(Stu::where('age>19')->order('age,desc')->getAll());
-		//dd(Stu::q('select * from stu'));
+		
 
 
 	}
 
-	public function add ()
-	{
-		//静态调用
-		//View::fetch();
-		//用于测试分离变量的变量
-		$a='nish';
-		//实例化调用分离变量并加载模板的方法with和fetch
-		//(new View())->with('a',$a)->fetch('index');
 
-	}
 
 }
